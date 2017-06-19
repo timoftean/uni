ba36eb6068ae141fdd4176d14f68529df6c2894e
Nicu <timofte.alexandru05@yahoo.com>
2017-06-20 00:07:24 +0300

    removed files

--- WEB/lab7/old/api/db_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define ('DB_HOST', "localhost");
-define ('DB_USER', "root");
-define ('DB_PASSWORD', "");
-define ('DB_DATABASE', "test");
-
-$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-?>
